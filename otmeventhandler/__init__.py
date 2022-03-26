ee64914156460257210c352da30d3b91e119e4f1
Stephen McMahon <stephen@slmcmahon.com>
2022-03-26 16:22:48 -0500

    Some refactoring

--- otmeventhandler/__init__.py
+++ otmeventhandler/__init__.py
@@ -1,17 +1,31 @@
 import json
 import openpyxl
+import logging
 
-from common.blobmanager import BlobManager
+from common.BlobManager import BlobManager
 import azure.functions as func
 
-
+# since this is a service bus triggered function, we get our 
+# queue message here as an argument to our main method
 def main(msg: func.ServiceBusMessage):
     # decode and convert response into a python dict
     qmsg = msg.get_body().decode('utf-8')
     msg = json.loads(qmsg)
 
+    # create an instance of our blob manager and extract the
+    # blob that matches the file name that we got from our 
+    # service bus message
     bm = BlobManager()
     bytes = bm.read_blob(msg['name'])
 
-    handler = __import__(f"common.{msg['type']}handler", fromlist=[''])
-    handler.process(openpyxl.load_workbook(bytes))
+    # locate the handler that corresponds to the type that we got
+    # from our service bus message and process the contents of the file
+    try:
+        class_name = f"{msg['type'].capitalize()}Processor"
+        module = __import__(f"common.{class_name}", fromlist=[''])
+        class_ = getattr(module, class_name)
+        handler = class_()
+        handler.process(xldocument=openpyxl.load_workbook(bytes))
+    except Exception as ex:
+        # need to do more here to ensure that we are aware of failures.
+        logging.error(ex)
--- xldata/__init__.py
+++ xldata/__init__.py
@@ -1,24 +1,27 @@
 import logging
 
-from common.blobmanager import BlobManager
-from common.servicebusmanager import ServiceBusManager
+from common.BlobManager import BlobManager
+from common.ServiceBusManager import ServiceBusManager
 import azure.functions as func
 
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
-    logging.info('Python HTTP trigger function processed a request.')
 
+    # load our form input parameters into variabless
     file = req.files.get('file')
     fileId = req.form.get('fileId')
     customerId = req.form.get('customerId')
     fileType = req.form.get('fileType')
 
+    # create instances of our helper classes
     bm = BlobManager()
     sbm = ServiceBusManager()
 
     try:
+        # write our file to blob storage
         bm.write_blob(file)
         
+        # create a message and enqueue it in our service bus
         message = {}
         message["id"] = fileId
         message["name"] = file.filename 
@@ -26,7 +29,8 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
         message["type"] = fileType
         sbm.enqueue(message)
         
+        # respond to the caller
         return func.HttpResponse(f"{file.filename} was uploaded successfully.")
     except Exception as ex:
         logging.info(ex)
-        return func.HttpResponse(ex)
+        return func.HttpResponse(ex.message)
