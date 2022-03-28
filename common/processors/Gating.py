22836f28cadb6dfb682d2f15d16be05108677d85
Stephen McMahon <stephen@slmcmahon.com>
2022-03-28 09:46:49 -0500

    some cleanup & refactoring

--- common/processors/Gating.py
+++ common/processors/Gating.py
@@ -4,10 +4,14 @@ class Gating:
     def __init__(self, xldocument):
         self.xldocument = xldocument
 
+    def validate(self):
+        return True
+
     def process(self):
         sheet = self.xldocument.active
 
         # for each row in the sheet, do something with the data
+        # min_row=2 causes the iterator to skip the first row
         for row in sheet.iter_rows(min_row=2):
             id = row[0].value
             locationId = row[1].value
--- otmeventhandler/__init__.py
+++ otmeventhandler/__init__.py
@@ -1,22 +1,27 @@
 import json
 import openpyxl
 import logging
+import os 
 
+from opencensus.ext.azure.log_exporter import AzureLogHandler
 from common.BlobManager import BlobManager
 import azure.functions as func
 
-# since this is a service bus triggered function, we get our 
+logger = logging.getLogger(__name__)
+logger.addHandler(AzureLogHandler(connection_string=os.environ["AppInsightsKey"]))
+# since this is a service bus triggered function, we get our
 # queue message here as an argument to our main method
 def main(msg: func.ServiceBusMessage):
     # decode and convert response into a python dict
     qmsg = msg.get_body().decode('utf-8')
     msg = json.loads(qmsg)
+    file_name = msg['name']
 
     # create an instance of our blob manager and extract the
-    # blob that matches the file name that we got from our 
+    # blob that matches the file name that we got from our
     # service bus message
     bm = BlobManager()
-    bytes = bm.read_blob(msg['name'])
+    bytes = bm.read_blob(file_name)
 
     # locate the handler that corresponds to the type that we got
     # from our service bus message and process the contents of the file
@@ -25,11 +30,17 @@ def main(msg: func.ServiceBusMessage):
         # in common.processors
         class_name = msg['type'].capitalize()
         module = __import__(f"common.processors.{class_name}", fromlist=[''])
+
         # dynamically load the class and pass in the workbook from blobstorage
         class_ = getattr(module, class_name)
         handler = class_(openpyxl.load_workbook(bytes))
-        # execute the process method -- all file processors must have a process() method
-        handler.process()
+
+        if handler.validate():
+            handler.process()
+            logger.info(f"Processing {file_name} for type {class_name}.")
+        else:
+            logger.error(f"Could not validate input for {class_name}.  File: {file_name}.")
+
     except Exception as ex:
         # need to do more here to ensure that we are aware of failures.
         logging.error(ex)
